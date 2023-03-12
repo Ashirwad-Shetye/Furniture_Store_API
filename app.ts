45d5c73d3a081736c0dc3eb2830f949e3a0b0f5c
Ashirwad P Shetye <99337222+Ashirwad-Shetye@users.noreply.github.com>
2023-03-12 23:45:21 +0530

    app setup

--- app.ts
+++ app.ts
@@ -1,1 +1,37 @@
-console.log('04 Store API')
+require('dotenv').config()
+
+// async errors
+
+const express = require('express');
+const app = express();
+
+const notFoundMiddleWare = require('./middleware/not-found')
+const errorMiddleWare = require('./middleware/error-handler')
+
+// middleware
+
+app.use(express.json())
+
+// routes
+
+app.get('/', (req : any, res : any) => { 
+    res.send('<h1>Furniture Store API</h1><a href="/api/v1/products">products route</a>');
+})
+
+// products routes
+
+app.use(notFoundMiddleWare);
+app.use(errorMiddleWare)
+
+const port = process.env.PORT || 5000
+
+const start = async () => {
+    try {
+        // connect 
+        app.listen( port, console.log(`server listening on port ${port}`))
+    } catch (err: any) { 
+
+    }
+}
+
+start()
